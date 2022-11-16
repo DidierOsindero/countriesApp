91666bf44fcd575ff2991a255cf3d8dd71414b43
Didier Osindero <didierosindero@gmail.com>
2022-11-16 13:36:14 +0000

    Add CountryData Interface

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,5 +1,20 @@
 import { CountryList } from "./CountryList";
 
+//SHAPE FOR API COUNTRY OBJECT
+interface CountryData {
+  [key:string]: unknown;
+  name: string;
+  capital: string;
+  continent: string;
+  population: string;
+  languages: LanguagesData[];
+}
+
+interface LanguagesData {
+  [key:string]: string;
+  name: string;
+}
+
 export const MainContent = (): JSX.Element => {
   return (
     <div className="mainContentWrapper">
