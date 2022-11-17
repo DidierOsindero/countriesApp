95d298c8644fc3c2ff84e4a285c90143f39d0bc0
Didier Osindero <didierosindero@gmail.com>
2022-11-17 11:04:47 +0000

    Made country props bold

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,5 +1,5 @@
 import { CountryData } from "./MainContent";
-
+import { MainContentStates } from "./MainContent"
 interface CountryProps {
   currentCountry: CountryData;
 }
@@ -11,12 +11,12 @@ export const Country = ({ currentCountry }: CountryProps): JSX.Element => {
     <div className="countryWrapper">
       <h3>{currentCountry.name}</h3>
       <div className="countryContentWrapper">
-        <img src={currentCountry.flags.png} width="150px"/>
-        <p>Population: {currentCountry.population}</p>
-        <p>Capital: {currentCountry.capital}</p>
-        <p>Continent: {currentCountry.region}</p>
-        <p>Main Language: {currentCountry.languages[0].name}</p>
-        {currentCountry.languages[1] && <p>Second Language: {currentCountry.languages[1].name}</p>}
+        <img src={currentCountry.flags.png} width="150px" alt=""/>
+        <p><b>Population:</b> {currentCountry.population}</p>
+        <p><b>Capital:</b> {currentCountry.capital}</p>
+        <p><b>Continent:</b> {currentCountry.region}</p>
+        <p><b>Main Language:</b> {currentCountry.languages[0].name}</p>
+        {currentCountry.languages[1] && <p><b>Second Language:</b> {currentCountry.languages[1].name}</p>}
       </div>
     </div>
   );
