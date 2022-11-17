84bcc3369db56477d02f07c3b348a0039ea48aca
Didier Osindero <didierosindero@gmail.com>
2022-11-17 17:44:17 +0000

    Change nav bar names

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ export const Header = (): JSX.Element => {
     <div className="headerWrapper">
       <h1>Countries of the World</h1>
       <div className="navBarWrapper">
-        <div>Home</div>
-        <div>About</div>
+        <div>Countries</div>
+        <div>Quiz</div>
       </div>
     </div>
   );
