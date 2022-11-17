74c7975b3369704ae473989e68688a2ed16cb95f
Didier Osindero <didierosindero@gmail.com>
2022-11-17 18:32:50 +0000

    Add input and image to Quiz page

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -23,8 +23,10 @@ interface MainContentProps {
   quizButton: boolean;
 }
 
-
-export const MainContent = ({homeButton, quizButton}: MainContentProps): JSX.Element => {
+export const MainContent = ({
+  homeButton,
+  quizButton,
+}: MainContentProps): JSX.Element => {
   //STATES
   const [searchText, setSearchText] = useState<string>("");
   const [countriesArray, setCountriesArray] = useState<CountryData[]>([]);
@@ -56,69 +58,75 @@ export const MainContent = ({homeButton, quizButton}: MainContentProps): JSX.Ele
 
   console.log("smallCountryFilter State: ", smallCountryFilter);
 
-  if (homeButton)
-  {return (
-    <div className="mainContentWrapper">
-      <div className="searchTools">
-        <div className="searchBarWrapper">
-          <input
-            value={searchText}
-            onChange={(el) => setSearchText(el.target.value)}
-          />
-        </div>
-        <div className="filterButtonsWrapper">
-          <button
-            onClick={handleBigCountryFilter}
-            className={
-              bigCountryFilter ? "bigCountryFilterPressed" : "bigCountryFilter"
-            }
-          >
-            Big Countries
-          </button>
-          <button
-            onClick={handleSmallCountryFilter}
-            className={
-              smallCountryFilter === true
-                ? "smallCountryFilterPressed"
-                : "smallCountryFilter"
-            }
-          >
-            Small Countries
-          </button>
-        </div>
-      </div>
-      <div className="countryListWrapper">
-        {countriesArray
-          .filter((country) => {
-            const isMatchingSearch = country.name
-              .toLowerCase()
-              .includes(searchText.toLowerCase());
-            const isBigCountry = Number(country.population) > 50000000;
-            const isSmallCountry = Number(country.population) < 5000000;
-            if (bigCountryFilter && smallCountryFilter) {
-              return isBigCountry && isSmallCountry && isMatchingSearch;
-            } else if (bigCountryFilter) {
-              return isBigCountry && isMatchingSearch;
-            } else if (smallCountryFilter) {
-              return isSmallCountry && isMatchingSearch;
-            } else {
-              return isMatchingSearch;
-            }
-          })
-          .map((el) => (
-            <Country currentCountry={el} key={el.name} />
-          ))}
-      </div>
-      <hr />
-    </div>
-  );} else {
+  if (homeButton) {
     return (
       <div className="mainContentWrapper">
+        <div className="searchTools">
+          <div className="searchBarWrapper">
+            <input
+              value={searchText}
+              onChange={(el) => setSearchText(el.target.value)}
+            />
+          </div>
+          <div className="filterButtonsWrapper">
+            <button
+              onClick={handleBigCountryFilter}
+              className={
+                bigCountryFilter
+                  ? "bigCountryFilterPressed"
+                  : "bigCountryFilter"
+              }
+            >
+              Big Countries
+            </button>
+            <button
+              onClick={handleSmallCountryFilter}
+              className={
+                smallCountryFilter === true
+                  ? "smallCountryFilterPressed"
+                  : "smallCountryFilter"
+              }
+            >
+              Small Countries
+            </button>
+          </div>
+        </div>
         <div className="countryListWrapper">
           {countriesArray
+            .filter((country) => {
+              const isMatchingSearch = country.name
+                .toLowerCase()
+                .includes(searchText.toLowerCase());
+              const isBigCountry = Number(country.population) > 50000000;
+              const isSmallCountry = Number(country.population) < 5000000;
+              if (bigCountryFilter && smallCountryFilter) {
+                return isBigCountry && isSmallCountry && isMatchingSearch;
+              } else if (bigCountryFilter) {
+                return isBigCountry && isMatchingSearch;
+              } else if (smallCountryFilter) {
+                return isSmallCountry && isMatchingSearch;
+              } else {
+                return isMatchingSearch;
+              }
+            })
             .map((el) => (
               <Country currentCountry={el} key={el.name} />
             ))}
+        </div>
+        <hr />
+      </div>
+    );
+  } else {
+    return (
+      <div className="quizWrapper">
+        <img
+          className="quizImage"
+          src={countriesArray[25].flags.png}
+          height="300px"
+        />
+        <div className="quizInputWrapper">
+          <input />
+          <input type="submit" />
         </div>
         <hr />
       </div>
