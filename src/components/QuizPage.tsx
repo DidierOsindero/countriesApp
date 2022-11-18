0e1720a26c7a79c3d58647c19cb9205db926c28f
Didier Osindero <didierosindero@gmail.com>
2022-11-18 13:48:38 +0000

    Create a random array for quiz and a next button

--- src/components/QuizPage.tsx
+++ src/components/QuizPage.tsx
@@ -6,10 +6,21 @@ interface QuizPageProps {
 }
 
 export const QuizPage = ({ countriesArray }: QuizPageProps): JSX.Element => {
+  //create a copy of countries array which can be manipulated in isolation for this page.
+  const quizArray = [...countriesArray];
+
   //STATES
   const [quizInputValue, setQuizInputValue] = useState<string>("");
   const [submittedQuizAnswer, setSubmittedQuizAnswer] = useState<string>("");
   const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null);
+
+  //Create a state to store random version of countries array
+  const [randomQuizArray] = useState<CountryData[]>(
+    quizArray.sort(() => Math.random() - 0.5)
+  );
+  const [questionNumber] = useState<number>(1);
+
+  console.log(randomQuizArray);
 
   //HANDLERS
   const handleAnswerSubmit = () => {
@@ -18,20 +29,23 @@ export const QuizPage = ({ countriesArray }: QuizPageProps): JSX.Element => {
   };
 
   if (
-    submittedQuizAnswer.toLowerCase() === countriesArray[25].name.toLowerCase()
+    submittedQuizAnswer.toLowerCase() ===
+    randomQuizArray[questionNumber].name.toLowerCase()
   ) {
     setSubmittedQuizAnswer("");
     setIsAnswerCorrect(true);
-    console.log(`Correct! ${countriesArray[25].name} is the correct answer `);
+    console.log(
+      `Correct! ${randomQuizArray[questionNumber].name} is the correct answer `
+    );
   } else if (
     submittedQuizAnswer.toLowerCase() !==
-      countriesArray[25].name.toLowerCase() &&
+      randomQuizArray[questionNumber].name.toLowerCase() &&
     submittedQuizAnswer.toLowerCase() !== ""
   ) {
     setSubmittedQuizAnswer("");
     setIsAnswerCorrect(false);
     console.log(
-      `Wrong Answer! ${countriesArray[25].name} is the correct answer `
+      `Wrong Answer! ${randomQuizArray[questionNumber].name} is the correct answer `
     );
   }
 
@@ -41,15 +55,22 @@ export const QuizPage = ({ countriesArray }: QuizPageProps): JSX.Element => {
       <h2>Name the Country</h2>
       <img
         className="quizImage"
-        src={countriesArray[25].flags.png}
+        src={randomQuizArray[questionNumber].flags.svg}
         height="300px"
         alt=""
       />
-      {isAnswerCorrect === true ? (
-        <p>Correct! {countriesArray[25].name} is the right answer. </p>
-      ) : (
-        <p>Wrong answer! {countriesArray[25].name} is the correct answer. </p>
+      {isAnswerCorrect === true && (
+        <p>
+          Correct! {randomQuizArray[questionNumber].name} is the right answer.{" "}
+        </p>
       )}
+      {isAnswerCorrect === false && (
+        <p>
+          Wrong answer! {randomQuizArray[questionNumber].name} is the correct
+          answer.{" "}
+        </p>
+      )}
+
       <div className="quizInputWrapper">
         <input
           value={quizInputValue}
@@ -62,6 +83,9 @@ export const QuizPage = ({ countriesArray }: QuizPageProps): JSX.Element => {
           onClick={handleAnswerSubmit}
           className="userAnswerSubmit"
         />
+        {isAnswerCorrect !== null && (
+          <button className="nextCountryButton">Next Country</button>
+        )}
       </div>
       <hr />
     </div>
