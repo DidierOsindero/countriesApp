b420272e7a04b887a44ad8df08a16fa8978023f8
Didier Osindero <didierosindero@gmail.com>
2022-11-22 07:32:35 +0000

    Improve quizResults CSS

--- src/components/QuizResultDisplay.tsx
+++ src/components/QuizResultDisplay.tsx
@@ -16,26 +16,26 @@ export const QuizResultDisplay = ({
       <button className="playAgainButton" onClick={handlePlayAgainButton}>
         Plag Again
       </button>
-      <h4>Correct Answers</h4>
+      <h4 className="correctAnswersText">Correct Answers</h4>
       <ul className="correctAnswersList">
         {correctAnswersArray.map((el) => {
           return (
-            <div key={el.name}>
+            <div className="correctQuizResultWrapper" key={el.name}>
               {" "}
-              <img src={el.flags.svg} alt="" height="40px" />
-              <p>{el.name}</p>
+              <img className="quizResultFlagIMG" src={el.flags.svg} alt=""  />
+              <p className="correctQuizResultFlagText">{el.name}</p>
             </div>
           );
         })}
       </ul>
-      <h4>Incorrect/Skipped Answers</h4>
+      <h4 className="incorrectAnswersText">Incorrect/Skipped Answers</h4>
       <ul className="incorrectAnswersList">
         {incorrectAnswersArray.map((el) => {
           return (
-            <div key={el.name}>
+            <div className="incorrectQuizResultWrapper" key={el.name}>
               {" "}
-              <img src={el.flags.svg} alt="" height="40px" />
-              <p>{el.name}</p>
+              <img className="quizResultFlagIMG" src={el.flags.svg} alt="" />
+              <p className="incorrectQuizResultFlagText">{el.name}</p>
             </div>
           );
         })}
