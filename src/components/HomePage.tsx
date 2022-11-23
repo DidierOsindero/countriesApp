a8d965ba246a4cbcae4a21665a2f1a7b648a0cfb
Didier Osindero <didierosindero@gmail.com>
2022-11-23 13:18:28 +0000

    Wait 3 seconds before fetch error

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -23,9 +23,12 @@ export const HomePage = ({ countriesArray }: HomePageProps): JSX.Element => {
   };
 
   //error message if data could not be fetched
-  if (countriesArray.length === 0) {
-    return <h3 className="fetchDataErrorMsg">{"Could Not Fetch Data :("}</h3>;
-  }
+  setTimeout(() => {
+    if (countriesArray.length === 0) {
+      return <h3 className="fetchDataErrorMsg">{"Could Not Fetch Data :("}</h3>;
+    }
+  }, 3000)
+  
 
   return (
     <div className="mainContentWrapper">
