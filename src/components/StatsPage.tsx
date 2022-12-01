efb16786c417f42cb5d101c91b6e19a2b42454aa
Didier Osindero <didierosindero@gmail.com>
2022-12-01 17:49:21 +0000

    Add number of quiz completions to statsPage

--- src/components/StatsPage.tsx
+++ src/components/StatsPage.tsx
@@ -1,9 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+interface IQuizCompletionTracker {
+  flags: number;
+  capitals: number;
+  population: number;
+}
+
 export const StatsPage = (): JSX.Element => {
-    return (
-      <>
-        <div className="statsPageWrapper">
-        <h2 className="statsPageTitle">Quiz Stats</h2>
-        </div>
-      </>
-    );
+  const [quizCompletionTracker, setQuizCompletionTracker] =
+    useState<IQuizCompletionTracker | null>(null);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  const fetchQuizCompletionTracker = async () => {
+    const response = await axios.get("http://localhost:4001/quiz-completions");
+    const data = response.data;
+    setQuizCompletionTracker(data);
   };
+
+  if (isFirstRender) {
+    fetchQuizCompletionTracker();
+    setIsFirstRender(false);
+  }
+
+
+  return (
+    <>
+      <div className="statsPageWrapper">
+        <h2 className="statsPageTitle">Quiz Stats</h2>
+        <p>Number of times you have completed the Flags Quiz: {quizCompletionTracker?.flags}</p>
+        <p>Number of times you have completed the Capitals Quiz: {quizCompletionTracker?.capitals}</p>
+        <p>Number of times you have completed the Populations Quiz: {quizCompletionTracker?.population}</p>
+      </div>
+    </>
+  );
+};
